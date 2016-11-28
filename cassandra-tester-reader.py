e97075f9ba4359d1579a435240fddace3e3fe5b6
gitaroktato <gitaroktato@gmail.com>
2016-11-28 11:11:18 +0100

    Fixed writer and reader issues

--- cassandra-tester-reader.py
+++ cassandra-tester-reader.py
@@ -1,18 +1,41 @@
 #!/usr/bin/python-pip
+import sys
 from cassandra.cluster import Cluster
+from cassandra.auth import PlainTextAuthProvider
+from cassandra.policies import DCAwareRoundRobinPolicy
+
 from time import time
 
-cluster = Cluster()
+# Parsing args
+hostname = sys.argv[1]
+username = sys.argv[2]
+password = sys.argv[3]
+
+# Logging in
+auth_provider = PlainTextAuthProvider(username=username, password=password)
+cluster = Cluster([hostname],
+    auth_provider=auth_provider,
+    load_balancing_policy=DCAwareRoundRobinPolicy(local_dc='AWS-AP-NORTHEAST'))
 session = cluster.connect('replicated')
 
 while True:
     result = []
     while len(result) <= 0:
         resultset = session.execute("""
-        select id, toUnixTimestamp(insertion_date) as insertion_date, toUnixTimestamp(now()) as now, some_data, WRITETIME (some_data) from replicated.test;
+        SELECT id, toUnixTimestamp(insertion_date) AS insertion_date, toUnixTimestamp(now()) AS now, some_data, WRITETIME (some_data) FROM replicated.test;
         """)
         result = resultset.current_rows
-
-
+    # Got results
     print "Lantency for query: %d" % (result[0].now - result[0].insertion_date)
-    session.execute("truncate replicated.test")
+    session.execute("""
+    UPDATE replicated.test_count
+         SET response_counter = response_counter + 1
+         WHERE id = {}
+    """.format(result[0].id))
+    # Waiting for next round
+    print 'Waiting for next round'
+    while len(result) > 0:
+        resultset = session.execute("""
+        SELECT id, toUnixTimestamp(insertion_date) AS insertion_date, toUnixTimestamp(now()) AS now, some_data, WRITETIME (some_data) FROM replicated.test;
+        """)
+        result = resultset.current_rows
--- cassandra-tester-writer.py
+++ cassandra-tester-writer.py
@@ -1,18 +1,50 @@
 #!/usr/bin/python-pip
+import sys
+import uuid
+
 from cassandra.cluster import Cluster
+from cassandra.auth import PlainTextAuthProvider
+from cassandra.policies import DCAwareRoundRobinPolicy
+from cassandra.query import SimpleStatement
+from cassandra import ConsistencyLevel
+
 from time import time
 
-cluster = Cluster()
+# Parsing args
+hostname = sys.argv[1]
+username = sys.argv[2]
+password = sys.argv[3]
+desired_response_counter = int(sys.argv[4])
+
+# Logging in
+auth_provider = PlainTextAuthProvider(username=username, password=password)
+cluster = Cluster([hostname],
+    auth_provider=auth_provider,
+    load_balancing_policy=DCAwareRoundRobinPolicy(local_dc='epam-by1'))
 session = cluster.connect('replicated')
+# Predefining queries
+truncate_test = SimpleStatement("TRUNCATE replicated.test",
+    consistency_level=ConsistencyLevel.ALL)
+truncate_test_count = SimpleStatement("TRUNCATE replicated.test_count",
+        consistency_level=ConsistencyLevel.ALL)
 
 while True:
+        # Inserting new test value
+        next_uuid = uuid.uuid1()
+        response_counter = 0
         session.execute("""
-        insert into replicated.test (id, insertion_date, some_data) values (uuid(), now(), 'dummy');
-        """)
-        table_size = 1
-
-        while table_size > 0:
+            INSERT INTO replicated.test (id, insertion_date, desired_response_counter, some_data)
+            VALUES ({}, now(), {}, 'dummy');
+        """.format(next_uuid, desired_response_counter))
+        # Waiting for all responses to arrive
+        while response_counter < desired_response_counter:
             rs = session.execute ("""
-            select count (*) as count from replicated.test
-            """)
-            table_size = rs[0].count
+                SELECT * FROM replicated.test_count WHERE id={}
+            """.format(next_uuid))
+            result = rs.current_rows
+            if len(result) > 0:
+                response_counter = result[0].response_counter
+        # Clearing results, preparing for next round
+        print 'Initiating next round'
+        session.execute(truncate_test)
+        session.execute(truncate_test_count)
